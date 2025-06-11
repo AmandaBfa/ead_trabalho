d03984b28fe2b00b2215c4765a6da44e7afa997b
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-06-11 15:09:13 -0300

    adicionando css ao projeto

--- templates/header.php
+++ templates/header.php
@@ -8,18 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Alunos</title>
     
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-
     <!-- Estilos personalizados -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+
+    <title>Alunos</title>
 </head>
 <body>
     <header> 
